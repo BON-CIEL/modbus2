584a9b36c262d782b97f8e97b475b4653cfa7bbd
Maxime Bonnevay <maximebonnevay.branly@gmail.com>
2025-10-16 10:17:49 +0200

    Commit #3

--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 using System.Windows.Forms;
 
 namespace modbus
 {
     public partial class Form1 : Form
     {
+        private Socket socket;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,22 +21,58 @@ namespace modbus
 
         private void IPtextBox1_TextChanged(object sender, EventArgs e)
         {
-
         }
 
         private void Connexionbutton1_Click(object sender, EventArgs e)
         {
-            this.AFFtextBox2.Text = this.IPtextBox1.Text;
+            try
+            {
+                string adresseIP = this.IPtextBox1.Text;
+                this.AFFtextBox2.Text += "Connexion au serveur " + adresseIP + "\r\n";
+
+                // Création de la socket
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Création de l'IPEndPoint (adresse IP + port Modbus 502)
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(adresseIP), 502);
+
+                // Connexion au serveur
+                socket.Connect(endPoint);
+
+                this.AFFtextBox2.Text += "Connexion ok\r\n";
+            }
+            catch (SocketException se)
+            {
+                this.AFFtextBox2.Text += "**Exception : Impossible de se connecter au serveur\r\n";
+                this.AFFtextBox2.Text += "Message : " + se.Message + "\r\n";
+            }
+            catch (Exception ex)
+            {
+                this.AFFtextBox2.Text += "**Exception : Impossible de se connecter au serveur\r\n";
+                this.AFFtextBox2.Text += "Message : " + ex.Message + "\r\n";
+            }
         }
 
         private void Deconnexionbutton2_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (socket != null)
+                {
+                    socket.Close();
+                    this.AFFtextBox2.Text += "Déconnexion réussie\r\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                this.AFFtextBox2.Text += "**Exception lors de la déconnexion\r\n";
+                this.AFFtextBox2.Text += "Message : " + ex.Message + "\r\n";
+            }
         }
 
         private void AFFtextBox2_TextChanged(object sender, EventArgs e)
         {
-
         }
     }
 }
+
