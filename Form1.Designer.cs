97cc4338354a31155fcb5be37c7cbb89425c375f
Maxime Bonnevay <maximebonnevay.branly@gmail.com>
2025-10-16 10:35:42 +0200

    Commit #4

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,11 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.Connexionbutton1 = new System.Windows.Forms.Button();
             this.Deconnexionbutton2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.IPtextBox1 = new System.Windows.Forms.TextBox();
             this.AFFtextBox2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1lire = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // Connexionbutton1
@@ -81,11 +86,46 @@
             this.AFFtextBox2.TabIndex = 4;
             this.AFFtextBox2.TextChanged += new System.EventHandler(this.AFFtextBox2_TextChanged);
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "tension moteur en Volt";
+            // 
+            // button1lire
+            // 
+            this.button1lire.Location = new System.Drawing.Point(253, 91);
+            this.button1lire.Name = "button1lire";
+            this.button1lire.Size = new System.Drawing.Size(75, 23);
+            this.button1lire.TabIndex = 7;
+            this.button1lire.Text = "Lire";
+            this.button1lire.UseVisualStyleBackColor = true;
+            this.button1lire.Click += new System.EventHandler(this.button1lire_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(334, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1lire);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.AFFtextBox2);
             this.Controls.Add(this.IPtextBox1);
             this.Controls.Add(this.label1);
@@ -106,6 +146,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox IPtextBox1;
         private System.Windows.Forms.TextBox AFFtextBox2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1lire;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,7 +16,6 @@ namespace modbus
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
         private void IPtextBox1_TextChanged(object sender, EventArgs e)
@@ -70,9 +69,64 @@ namespace modbus
             }
         }
 
+        private void button1lire_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Trame Modbus TCP pour lire l'adresse 3207 (fonction 03 - Read Holding Registers)
+                // Structure de la trame:
+                // - Transaction ID: 0x0000 (2 octets)
+                // - Protocol ID: 0x0000 (2 octets) - Modbus
+                // - Length: 0x0006 (2 octets) - 6 octets suivent
+                // - Unit ID: 0x01 (1 octet) - Device ID
+                // - Function: 0x03 (1 octet) - Read Holding Registers
+                // - Start Address: 0x0C86 (2 octets) - Adresse 3206 (3207-1 car décalage Modbus)
+                // - Quantity: 0x0001 (2 octets) - Lire 1 registre (2 octets)
+
+                var trameE = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x06, 0x01, 0x03, 0x0C, 0x86, 0x00, 0x01 };
+
+                // Envoi de la trame
+                socket.Send(trameE);
+
+                // Réception de la réponse
+                var trameR = new byte[256];
+                int nbOctets = socket.Receive(trameR);
+
+                // Affichage de la trame reçue en hexadécimal
+                this.AFFtextBox2.Text += "Trame reçue (" + nbOctets + " octets): ";
+                for (int i = 0; i < nbOctets; i++)
+                {
+                    this.AFFtextBox2.Text += String.Format("{0:X2} ", trameR[i]);
+                }
+                this.AFFtextBox2.Text += "\r\n";
+
+                // Extraction de la valeur (octets 9 et 10 de la trame)
+                // La valeur est en Big Endian (octet de poids fort en premier)
+                int valeurBrute = (trameR[9] << 8) | trameR[10];
+
+                // Calcul de la tension : valeur / 10 (selon la documentation)
+                double tension = valeurBrute / 10.0;
+
+                // Affichage dans la textBox1 (textBox pour afficher la tension)
+                this.textBox1.Text = String.Format("{0:F1}", tension);
+
+                this.AFFtextBox2.Text += String.Format("Valeur brute: {0} => Tension: {1:F1} V\r\n", valeurBrute, tension);
+            }
+            catch (Exception ex)
+            {
+                this.AFFtextBox2.Text += "**Exception lors de la lecture\r\n";
+                this.AFFtextBox2.Text += "Message : " + ex.Message + "\r\n";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
         private void AFFtextBox2_TextChanged(object sender, EventArgs e)
         {
         }
     }
 }
+
 
