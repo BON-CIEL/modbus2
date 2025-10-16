2b45dc44da7c9b336998c2d290956f637f6eed0a
Maxime Bonnevay <maximebonnevay.branly@gmail.com>
2025-10-16 11:11:32 +0200

    Commit #5

--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,18 @@
 ﻿using System;
-using System.Net;
-using System.Net.Sockets;
 using System.Windows.Forms;
 
 namespace modbus
 {
     public partial class Form1 : Form
     {
-        private Socket socket;
+        // Déclaration de l'objet CModbus
+        private CModbus modbus;
 
         public Form1()
         {
             InitializeComponent();
+            // Création de l'objet CModbus
+            modbus = new CModbus();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -24,98 +25,61 @@ namespace modbus
 
         private void Connexionbutton1_Click(object sender, EventArgs e)
         {
-            try
+            string adresseIP = this.IPtextBox1.Text;
+            this.AFFtextBox2.Text += "Connexion au serveur " + adresseIP + "\r\n";
+
+            // Appel de la méthode Connexion de la classe CModbus
+            string resultat = modbus.Connexion(adresseIP);
+
+            if (resultat == "ok")
             {
-                string adresseIP = this.IPtextBox1.Text;
-                this.AFFtextBox2.Text += "Connexion au serveur " + adresseIP + "\r\n";
-
-                // Création de la socket
-                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-                // Création de l'IPEndPoint (adresse IP + port Modbus 502)
-                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(adresseIP), 502);
-
-                // Connexion au serveur
-                socket.Connect(endPoint);
-
                 this.AFFtextBox2.Text += "Connexion ok\r\n";
             }
-            catch (SocketException se)
+            else
             {
                 this.AFFtextBox2.Text += "**Exception : Impossible de se connecter au serveur\r\n";
-                this.AFFtextBox2.Text += "Message : " + se.Message + "\r\n";
-            }
-            catch (Exception ex)
-            {
-                this.AFFtextBox2.Text += "**Exception : Impossible de se connecter au serveur\r\n";
-                this.AFFtextBox2.Text += "Message : " + ex.Message + "\r\n";
+                this.AFFtextBox2.Text += "Message : " + resultat + "\r\n";
             }
         }
 
         private void Deconnexionbutton2_Click(object sender, EventArgs e)
         {
-            try
+            // Appel de la méthode Deconnexion de la classe CModbus
+            string resultat = modbus.Deconnexion();
+
+            if (resultat == "ok")
             {
-                if (socket != null)
-                {
-                    socket.Close();
-                    this.AFFtextBox2.Text += "Déconnexion réussie\r\n";
-                }
+                this.AFFtextBox2.Text += "Déconnexion réussie\r\n";
             }
-            catch (Exception ex)
+            else
             {
                 this.AFFtextBox2.Text += "**Exception lors de la déconnexion\r\n";
-                this.AFFtextBox2.Text += "Message : " + ex.Message + "\r\n";
+                this.AFFtextBox2.Text += "Message : " + resultat + "\r\n";
             }
         }
 
         private void button1lire_Click(object sender, EventArgs e)
         {
-            try
+            string strResultat = "";
+
+            // Lecture de la tension à l'adresse 3207 avec la méthode LireUnMot
+            Int16 valeurBrute = modbus.LireUnMot(3207, ref strResultat);
+
+            if (strResultat == "ok")
             {
-                // Trame Modbus TCP pour lire l'adresse 3207 (fonction 03 - Read Holding Registers)
-                // Structure de la trame:
-                // - Transaction ID: 0x0000 (2 octets)
-                // - Protocol ID: 0x0000 (2 octets) - Modbus
-                // - Length: 0x0006 (2 octets) - 6 octets suivent
-                // - Unit ID: 0x01 (1 octet) - Device ID
-                // - Function: 0x03 (1 octet) - Read Holding Registers
-                // - Start Address: 0x0C86 (2 octets) - Adresse 3206 (3207-1 car décalage Modbus)
-                // - Quantity: 0x0001 (2 octets) - Lire 1 registre (2 octets)
-
-                var trameE = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x06, 0x01, 0x03, 0x0C, 0x86, 0x00, 0x01 };
-
-                // Envoi de la trame
-                socket.Send(trameE);
-
-                // Réception de la réponse
-                var trameR = new byte[256];
-                int nbOctets = socket.Receive(trameR);
-
-                // Affichage de la trame reçue en hexadécimal
-                this.AFFtextBox2.Text += "Trame reçue (" + nbOctets + " octets): ";
-                for (int i = 0; i < nbOctets; i++)
-                {
-                    this.AFFtextBox2.Text += String.Format("{0:X2} ", trameR[i]);
-                }
-                this.AFFtextBox2.Text += "\r\n";
-
-                // Extraction de la valeur (octets 9 et 10 de la trame)
-                // La valeur est en Big Endian (octet de poids fort en premier)
-                int valeurBrute = (trameR[9] << 8) | trameR[10];
-
-                // Calcul de la tension : valeur / 10 (selon la documentation)
+                // Calcul de la tension : valeur / 10
                 double tension = valeurBrute / 10.0;
 
-                // Affichage dans la textBox1 (textBox pour afficher la tension)
+                // Affichage dans la textBox1
                 this.textBox1.Text = String.Format("{0:F1}", tension);
 
-                this.AFFtextBox2.Text += String.Format("Valeur brute: {0} => Tension: {1:F1} V\r\n", valeurBrute, tension);
+                // Affichage dans la zone de statut
+                this.AFFtextBox2.Text += String.Format("Tension: {0:F1} V\r\n", tension);
             }
-            catch (Exception ex)
+            else
             {
                 this.AFFtextBox2.Text += "**Exception lors de la lecture\r\n";
-                this.AFFtextBox2.Text += "Message : " + ex.Message + "\r\n";
+                this.AFFtextBox2.Text += "Message : " + strResultat + "\r\n";
             }
         }
 
---
+++ modbus.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Net;           // pour IPEndPoint
+using System.Net.Sockets;   // pour les sockets
+
+namespace modbus
+{
+    
+    class CModbus
+    {
+              private Socket socket; // Socket pour la communication TCP/IP
+
+        public CModbus()
+        {
+            socket = null;
+        }
+
+      
+        public String Connexion(String adresseIP)
+        {
+            try
+            {
+                // Création de la socket
+                // - AddressFamily.InterNetwork : IPV4
+                // - SocketType.Stream : mode connecté (TCP)
+                // - ProtocolType.Tcp : protocole TCP
+                socket = new Socket(AddressFamily.InterNetwork,
+                                   SocketType.Stream,
+                                   ProtocolType.Tcp);
+
+                // Création du point d'accès distant (adresse IP + port)
+                // Port Modbus TCP/IP : 502
+                IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(adresseIP), 502);
+
+                // Connexion au serveur
+                socket.Connect(ipEndPoint);
+
+                return "ok";
+            }
+            catch (System.Exception ex)
+            {
+                // Retourne le message d'erreur
+                return ex.Message;
+            }
+        }
+
+        
+        public String Deconnexion()
+        {
+            try
+            {
+                // Vérification que la socket existe
+                if (socket != null)
+                {
+                    // Fermeture de la socket
+                    socket.Close();
+                }
+
+                return "ok";
+            }
+            catch (System.Exception ex)
+            {
+                // Retourne le message d'erreur
+                return ex.Message;
+            }
+        }
+
+       
+        public Int16 LireUnMot(Int16 adresse, ref String strResultat)
+        {
+            try
+            {
+               
+                // Attention : il faut soustraire 1 à l'adresse (décalage Modbus)
+                Int16 adresseModbus = (Int16)(adresse + 0);
+
+                // Conversion de l'adresse en 2 octets (Big Endian)
+                byte adresseHaut = (byte)(adresseModbus >> 8);    // Octet de poids fort
+                byte adresseBas = (byte)(adresseModbus & 0xFF);   // Octet de poids faible
+
+                // Construction de la trame
+                var trameE = new byte[]
+                {
+                    0x00, 0x00,      // Transaction Identifier
+                    0x00, 0x00,      // Protocol Identifier (0 = Modbus)
+                    0x00, 0x06,      // Length (6 octets suivent)
+                    0x01,            // Unit Identifier (Device ID)
+                    0x03,            // Function code (03 = Read Holding Registers)
+                    adresseHaut,     // Adresse haute
+                    adresseBas,      // Adresse basse
+                    0x00, 0x01       // Nombre de registres à lire (1)
+                };
+
+                
+                socket.Send(trameE);
+
+                
+                var trameR = new byte[256];
+                int nbBytesLus = socket.Receive(trameR);
+
+                // Vérification que la réponse est valide (au moins 11 octets)
+                if (nbBytesLus < 11)
+                {
+                    strResultat = "Erreur : trame trop courte";
+                    return 0;
+                }
+
+               
+                // Structure de la réponse :
+                // [0-1] Transaction ID
+                // [2-3] Protocol ID
+                // [4-5] Length
+                // [6] Unit ID
+                // [7] Function code
+                // [8] Byte count
+                // [9-10] Data (2 octets en Big Endian)
+
+                byte octetPoidsFort = trameR[9];
+                byte octetPoidsFaible = trameR[10];
+
+                // Reconstitution de la valeur 16 bits
+                Int16 valeur = (Int16)((octetPoidsFort << 8) | octetPoidsFaible);
+
+                strResultat = "ok";
+                return valeur;
+            }
+            catch (System.Exception ex)
+            {
+                // En cas d'erreur, retourne le message d'exception
+                strResultat = ex.Message;
+                return 0;
+            }
+        }
+
+ 
+     
+    }
+}
+
+
